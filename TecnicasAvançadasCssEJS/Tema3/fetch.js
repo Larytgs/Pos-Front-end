cb40150e235fcb0d5a19d59c64c738e06cc522e7
Larytgs <149098968+Larytgs@users.noreply.github.com>
2024-09-17 17:19:09 -0300

    att

--- TecnicasAvançadasCssEJS/Tema3/fetch.js
+++ TecnicasAvançadasCssEJS/Tema3/fetch.js
@@ -2,28 +2,28 @@
 let respon = fetch(URL);
 
 //Retornando promise
-fetch(URL)
+/*fetch(URL)
     .then{response() => {
 
     }}
     .catch(error => {
-
-    });
+        
+    });*/
 
 //Lendo resposta
-fetch{'/readme.txt'}
+//fetch{'/readme.txt'}
     let response = await fetch('/readme.txt');
     let data = await response.text();
     console.log(data);
 
 //Manipulando código de resposta
 async function fetchText() {
-    let response = await fetch("readem.txt");
+    let responsee = await fetch("readem.txt");
 
-    console.log(response.status); //200
-    console.log(response.statusText); //OK
+    console.log(responsee.status); //200
+    console.log(responsee.statusText); //OK
 
-    if (response.status === 200) {
+    if (responsee.status === 200) {
         let data = await response.text();
         //handie data
     }
